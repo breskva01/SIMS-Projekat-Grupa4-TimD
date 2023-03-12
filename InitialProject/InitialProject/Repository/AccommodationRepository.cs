6114889c399e3e62ca9f89edb2d5de56c241d1b6
VukM <vukmacanovic@gmail.com>
2023-03-12 22:32:21 +0100

    Squashed commit of the following:
    commit dd502add38869fc2b78bd887204a92452c02800b
    Author: VukM <vukmacanovic@gmail.com>
    Date:   Sun Mar 12 14:57:43 2023 +0100
    
        fix: Property name changed
    
    commit 97959df048c9b0a7e80763ff3e19e6a79caf4e52
    Author: VukM <vukmacanovic@gmail.com>
    Date:   Sun Mar 12 14:15:46 2023 +0100
    
        feat: Added all accommodation types filter
    
    commit 16dd62cdebb3b2bd021835e524b5b6289c6974dd
    Author: VukM <vukmacanovic@gmail.com>
    Date:   Sat Mar 11 15:04:24 2023 +0100
    
        feat: AccommodationBrowser implemented
    
    commit 7d27e1524ef9e9abef3fc7bda5477a0b3e0500da
    Author: VukM <vukmacanovic@gmail.com>
    Date:   Sat Mar 11 14:00:08 2023 +0100
    
        feat: Application changed to MVC architecture
    
    commit 18748f9ba3b20accd873364c057bd2070555d17d
    Merge: ddabe8d 13f7baa
    Author: VukM <vukmacanovic@gmail.com>
    Date:   Sat Mar 11 10:12:42 2023 +0100
    
        Merge branch 'feature-register-accomodation' into feature-accommodation-browser
    
    commit ddabe8d226bd412f5e54798af72379611a212405
    Author: VukM <vukmacanovic@gmail.com>
    Date:   Sat Mar 11 10:03:19 2023 +0100
    
        feat: AccommodationBrowser layout created
    
    commit 13f7baacb38b447dbc77b148d306e8806a92b861
    Author: Nemanja Tasovac <ntasovac01@gmail.com>
    Date:   Fri Mar 10 18:24:19 2023 +0100
    
        feat: added observer and other folders
    
    commit b1ca32d5b9ee8f4ee178fe6faae213b3fe99e3bb
    Author: VukM <vukmacanovic@gmail.com>
    Date:   Wed Mar 8 13:21:35 2023 +0100
    
        feat: AccommodationRepository added
    
    commit 4c796b57c19e2604d1899a8b2e5f971b20910843
    Author: VukM <vukmacanovic@gmail.com>
    Date:   Tue Mar 7 20:40:14 2023 +0100
    
        feat: Accommodation model added

---
+++ InitialProject/InitialProject/Repository/AccommodationRepository.cs
@@ -0,0 +1,57 @@
+﻿using InitialProject.Model;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Repository
+{
+    class AccommodationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/accommodations.csv";
+
+        private readonly Serializer<Accommodation> _serializer;
+
+        private List<Accommodation> _accommodations;
+
+        public AccommodationRepository()
+        {
+            _serializer = new Serializer<Accommodation>();
+            _accommodations = _serializer.FromCSV(FilePath);
+        }
+
+        public List<Accommodation> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public Accommodation Save(Accommodation accommodation)
+        {
+            accommodation.Id = NextId();
+            _accommodations = _serializer.FromCSV(FilePath);
+            _accommodations.Add(accommodation);
+            _serializer.ToCSV(FilePath, _accommodations);
+            return accommodation;
+        }
+
+        public int NextId()
+        {
+            _accommodations = _serializer.FromCSV(FilePath);
+            if (_accommodations.Count < 1)
+            {
+                return 1;
+            }
+            return _accommodations.Max(a => a.Id) + 1;
+        }
+
+        public void Delete(Accommodation accommodation)
+        {
+            _accommodations = _serializer.FromCSV(FilePath);
+            Accommodation founded = _accommodations.Find(a => a.Id == accommodation.Id);
+            _accommodations.Remove(founded);
+            _serializer.ToCSV(FilePath, _accommodations);
+        }
+    }
+}
