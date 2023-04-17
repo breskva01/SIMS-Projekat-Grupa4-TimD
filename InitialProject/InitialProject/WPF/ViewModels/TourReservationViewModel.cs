0f64a283018fc83de8db75c22c57fceb59163e7f
spasoje2001 <spasoje.brboric@gmail.com>
2023-04-17 03:20:44 +0200

    refactor: Tour reservation switched to MVVM

--- InitialProject/InitialProject/WPF/ViewModels/TourReservationViewModel.cs
+++ InitialProject/InitialProject/WPF/ViewModels/TourReservationViewModel.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace InitialProject.WPF.ViewModels
@@ -15,8 +16,10 @@ namespace InitialProject.WPF.ViewModels
     public class TourReservationViewModel : ViewModelBase
     {
         private User _user;
-        public Tour _tour;
+        public Tour SelectedTour { get; set; }
+        public string AvailableSpots { get; set; }
         private readonly NavigationStore _navigationStore;
+
         private string _numberOfGuests;
         public  string NumberOfGuests
         {
@@ -30,7 +33,23 @@ namespace InitialProject.WPF.ViewModels
                 OnPropertyChanged(nameof(NumberOfGuests));
             }
         }
-      
+
+        private int GetNumberOfGuests()
+        {
+            int numberOfGuests = 0;
+
+            try
+            {
+                numberOfGuests = int.Parse(NumberOfGuests);
+            }
+            catch
+            {
+                MessageBox.Show("You entered a non-number value for number of guests.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+
+            return numberOfGuests;
+        }
+
         public ICommand ReserveCommand { get; }
         public ICommand CancelCommand { get; }
 
@@ -38,10 +57,37 @@ namespace InitialProject.WPF.ViewModels
         {
             _navigationStore = navigationStore;
             _user = user;
-            _tour = tour;
-            //ReserveCommand = new MakeReservationCommand(this, tourBrowserNavigationService);
+            SelectedTour = tour;
+            AvailableSpots = (SelectedTour.MaximumGuests - SelectedTour.CurrentNumberOfGuests).ToString();
+            CancelCommand = new ExecuteMethodCommand(ShowTourBrowserView);
+            ReserveCommand = new ExecuteMethodCommand(MakeReservation);
             //CancelCommand = new NavigateCommand(tourBrowserNavigationService);
         }
 
+        private void ShowTourBrowserView()
+        {
+            TourBrowserViewModel tourBrowserViewModel = new TourBrowserViewModel(_navigationStore, _user);
+            NavigateCommand navigate = new NavigateCommand(new NavigationService(_navigationStore, tourBrowserViewModel));
+            navigate.Execute(null);
+        }
+        private void MakeReservation()
+        {
+            int numberOfGuests = GetNumberOfGuests();
+            if (numberOfGuests == 0)
+            {
+                MessageBox.Show("Please input a number of guests first.", "Warning", MessageBoxButton.OK, MessageBoxImage.Hand);
+                return;
+            }
+
+            if (numberOfGuests + SelectedTour.CurrentNumberOfGuests > SelectedTour.MaximumGuests)
+            {
+                MessageBox.Show("Unfortunately, there is not enough available spots for that many guests. Try lowering the guest number.", "Warning", MessageBoxButton.OK, MessageBoxImage.Hand);
+                return;
+            }
+
+            //TourReservation tourReservation = _reservationService.CreateReservation(SelectedTour.Id, _user.Id, numberOfGuests);
+
+            ShowTourBrowserView();
+        }
     }
 }
