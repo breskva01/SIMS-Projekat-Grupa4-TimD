cf7bcd0dec3abce1028e82eac5d21e6b591ec285
VukM <vukmacanovic@gmail.com>
2023-04-19 15:59:45 +0200

    fix: Notification logic

--- InitialProject/InitialProject/Repositories/AccommodationReservationMoveRequestRepository.cs
+++ InitialProject/InitialProject/Repositories/AccommodationReservationMoveRequestRepository.cs
@@ -51,7 +51,8 @@ namespace InitialProject.Repositories
         }
         private void UpdateGuestNotifiedField(int guestId)
         {
-            _requests.FindAll(request => request.Reservation.GuestId == guestId)
+            _requests.FindAll(request => request.Reservation.GuestId == guestId &&
+                                         request.Status != ReservationMoveRequestStatus.Pending)
                      .ForEach(request => request.GuestNotified = true);
             _fileHandler.Save(_requests);
         }
